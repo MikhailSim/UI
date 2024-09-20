8a272bfa3ae02bd718ee77d3a8e75ed15c1f36fc
Михаил Антонов <mihailantonov@MacBook-Pro-Mihail.local>
2024-09-20 09:51:33 +0400

    Fix SessionNotCreatedException by installing Google Chrome

--- conftest.py
+++ conftest.py
@@ -4,20 +4,22 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 @pytest.fixture(scope="function")
 def setup():
     chrome_options = Options()
+
     # Убираем headless режим, чтобы окно браузера было видно
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
 
-    # Создаем экземпляр веб-драйвера
+    # Создаем экземпляр веб-драйвера с автоматической установкой ChromeDriver
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     # Устанавливаем начальный URL
     driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager")
 
-    # Ожидание, чтобы страница полностью загрузилась
+    # Устанавливаем ожидание для полной загрузки страницы
     driver.implicitly_wait(10)
 
     yield driver  # Возвращаем драйвер для использования в тестах
