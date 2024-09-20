5ec07f4f43f946c8255ed83d79317c55c982a289
Михаил Антонов <mihailantonov@MacBook-Pro-Mihail.local>
2024-09-20 11:53:04 +0400

    Fix Chrome headless configuration for CI

--- conftest.py
+++ conftest.py
@@ -4,11 +4,19 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 @pytest.fixture(scope="function")
 def setup():
     chrome_options = Options()
+
+    # Включаем режим headless для CI/CD
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Указываем разрешение экрана (особенно важно для безголового режима)
+    chrome_options.add_argument("window-size=1920,1080")
 
     # Создаем экземпляр веб-драйвера с автоматической установкой ChromeDriver
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
